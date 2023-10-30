29417fce512c0acf0a063a5029ba1736fc77a83e
Mohammad Yousry <mohammadyousry187@gmail.com>
2023-10-30 18:38:53 +0200

    Class Station

---
+++ app/src/main/java/com/professional/witl/entities/Station.java
@@ -0,0 +1,13 @@
+package com.professional.witl.entities;
+
+import java.util.List;
+
+public class Station {
+    private String sid;
+    private String nameEN;
+    private String nameAR;
+    private String geoLocation;
+    private List<String> tramLines;
+
+    // Constructors, getters, and setters
+}
