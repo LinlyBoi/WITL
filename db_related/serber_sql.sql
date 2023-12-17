2d785c884e452fb0fe3b4840d5ccb1b2940d1da1
LinlyBoi <libkyy@e.email>
2023-12-17 22:19:44 +0200

    added direction, removed fill_status

--- db_related/serber_sql.sql
+++ db_related/serber_sql.sql
@@ -54,11 +54,11 @@ CREATE TABLE prefers(pref_UID SERIAL,
 -- time_of_day => Time object, nanosecond accuracy, y'all handle it idk
 -- week_day    => varchar as {Mon, Tue, Wed}, Could probs just encode with 0 -> 6
 -- tram_line   => int ranging 1 -> 4
--- fill_status => Boolean, 1 full, 0 not as full
+-- direction => Boolean, 1 full, 0 not as full
 CREATE TABLE arrival(time_of_day TIME,
                      week_day INTEGER,
                      tram_line INTEGER,
-                     fill_status BOOLEAN
+                     direction BOOLEAN
                      CONSTRAINT ck PRIMARY KEY (week_day,tram_line)
                      CONSTRAINT in_week CHECK (week_day >= 0 AND week_day <= 6)
                      CONSTRAINT valid_line CHECK (tram_line >= 1 AND tram_line <= 4)
